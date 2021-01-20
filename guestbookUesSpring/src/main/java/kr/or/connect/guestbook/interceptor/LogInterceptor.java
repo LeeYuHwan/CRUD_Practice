525fb6c0e2f64900c990d6d1af565389ea5ac3a8
LeeYuHwan <dbghksskfk@naver.com>
2021-01-21 01:40:40 +0900

    slf4jLogger

--- guestbookUesSpring/src/main/java/kr/or/connect/guestbook/interceptor/LogInterceptor.java
+++ guestbookUesSpring/src/main/java/kr/or/connect/guestbook/interceptor/LogInterceptor.java
@@ -7,15 +7,18 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class LogInterceptor extends HandlerInterceptorAdapter {
+	//private Logger logger = LoggerFactory.getLogger(this.getClass());
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
+		//logger.debug("{} 가 종료되었습니다. {} 를 view로 사용합니다.", handler.toString(), modelAndView.getViewName());
 		System.out.println(handler.toString() + " 가 종료되었습니다.  " + modelAndView.getViewName() + "을 view로 사용합니다.");
 	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		//logger.debug("{} 를 호출했습니다.", handler.toString());
 		System.out.println(handler.toString() + " 를 호출했습니다.");
 		return true;
 	}
